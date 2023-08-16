9c346f1975217136e6bcf90be19e2fb852794922
AnargyaDebug <73513088+AnargyaDebug@users.noreply.github.com>
2023-08-16 19:44:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
 import discord
-from bot_logic import gen_pass
+from discord.ext import commands
+from bot_logic import random_word
 import random
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix=";", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Kita telah masuk sebagai {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$halo'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
-    elif message.content.startswith("$what"):
-        await message.channel.send("I am a chatbot!")
-    elif message.content.startswith("$why"):
-        await message.channel.send("I am just a test subject :(")
-    elif message.content.startswith("$commands"):
-        await message.channel.send("Commands are: $halo, $bye, $what, $why, $flip-coin, $gibberish")
-    elif message.content.startswith("$flip-coin"):
-        rand = random.randint(0, 1)
-        result = "Heads" if rand == 0 else "Tails"
-        await message.channel.send(result)
-    elif message.content.startswith("$gibberish"):
-        gibberish_list = ["djdjeo a[apdpe ododekoed] 6994797#7485943 )($&R983 djfhdjfhjrf)",
-                          "saofjkefjekfjefj",
-                          "ijfwiofu34 -04083499349034",
-                          "orfjkfjjrfjrf",
-                          "E",
-                          "🦍"]
-        result = random.choice(gibberish_list)
-        await message.channel.send(result)
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
 
-        if result == "E":
-            await message.channel.send("YOU HAVE ENCOUNTERED E")
-        elif result == "🦍":
-            await message.channel.send("YOU HAVE ENCOUNTERED THE GORILLA")
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def bye(ctx):
+    await ctx.send("B-bye")
 
-client.run("MTEzODgxMzA1NDUzMjk5NzIzMg.GGhian.heTG_r2P2rQ_UDfC1gFkCzBHV1JtHhqP8Xn--Q")
+@bot.command()
+async def what(ctx):
+    await ctx.send("I'm a chatbot!")
+
+@bot.command()
+async def why(ctx):
+    await ctx.send("I'm just a test subject :(")
+
+@bot.command()
+async def commands(ctx):
+    await ctx.send("Commands are: ;halo, ;bye, ;what, ;why, ;flip-coin, ;gibberish, ;pop <amount>")
+
+@bot.command()
+async def flipcoin(ctx):
+    rand = random.randint(0, 1)
+    result = "Heads" if rand == 0 else "Tails"
+    await ctx.send(result)
+
+@bot.command()
+async def gibberish(ctx, length=10):
+    result = random.choice([random_word(length), random_word(length), random_word(length), "E", "🦍"])
+    await ctx.send(result)
+
+    if result == "E":
+        await ctx.send("YOU HAVE ENCOUNTERED E")
+    elif result == "🦍":
+        await ctx.send("YOU HAVE ENCOUNTERED THE GORILLA")
+
+bot.run("MTEzODgxMzA1NDUzMjk5NzIzMg.GGhian.heTG_r2P2rQ_UDfC1gFkCzBHV1JtHhqP8Xn--Q")
